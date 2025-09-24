fc633cccc9dec17bca2a75eb7cfa5ce99f6e8909
PRPL5 <jakub.demaliaj@gmail.com>
2025-09-24 18:19:39 +0200

    Database connection setup

---
+++ server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import { v4 as uuidv4 } from "uuid";
+import urlRoutes from "./routes/urlRoutes.js";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+app.use((req, res, next) => {
+  if (!req.cookies.user_id) {
+    res.cookie("user_id", uuidv4(), { httpOnly: true });
+  }
+  next();
+});
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use("/", urlRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
+
+
+app.get('/short.link/:shortUrl', (req, res) => {
+  const shortUrl = req.params.shortUrl;
+  const originalUrl = urlMap[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.status(404).send('URL not found');
+  }
+});
